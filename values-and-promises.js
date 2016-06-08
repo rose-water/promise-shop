fa3fc5c65049fb7702968adf209c863e215ba0be
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-06-08 09:50:12 -0700

    syntax cleanup

--- values-and-promises.js
+++ values-and-promises.js
@@ -10,6 +10,10 @@ function attachTitle(name) {
 // 	return attachTitle(lastName);
 // }).then(console.log);
 
-function attachTitle(name) { return 'DR. ' + name; }
-var promise = new Promise(function (fulfill, reject) { fulfill('MANHATTAN'); });
+function attachTitle(name) { 
+	return 'DR. ' + name; 
+}
+var promise = new Promise(function (fulfill, reject) { 
+	fulfill('MANHATTAN'); });
+
 promise.then(attachTitle).then(console.log);
